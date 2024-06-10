fa983ecb9444a61e586c5b8ae90fc76103538f41
Toan Nguyen Bao <toan.nguyen@titancorp.vn>
2024-06-10 10:22:56 +0700

    rename folder navBar

---
+++ clone-trello/src/components/navBar/index.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { Dropdown } from "react-bootstrap";
+import "./style.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faCircleQuestion } from "@fortawesome/free-regular-svg-icons";
+import * as constants from "../../shared/constants";
+
+const NavBar = () => {
+  return (
+    <React.Fragment>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <div className="ms-3">
+            <a class="navbar-brand fs-4 fw-bolder" style={{ color: "#455570" }}>
+              Trellone
+            </a>
+          </div>
+
+          <div className="d-flex flex-fill gap-2">
+            <div>
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic" variant="light">
+                  Workspaces
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">
+                    Your Workspaces
+                  </Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item href="#/action-2">Workspaces 1</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Workspaces 2</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div>
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic" variant="light">
+                  Recent
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-2">Workspaces 1</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Workspaces 2</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div>
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic" variant="light">
+                  Starred
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div>
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic" variant="light">
+                  More
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-2">Template 1</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Template 2</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div>
+              <button type="button" class="btn btn-primary">
+                <span className="fw-semibold">Create</span>
+              </button>
+            </div>
+          </div>
+
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </form>
+          <div className="ms-2 d-flex gap-3">
+            <div>
+              <FontAwesomeIcon icon={faBell} size="lg" color="#909191" />
+            </div>
+            <div>
+              <FontAwesomeIcon
+                icon={faCircleQuestion}
+                size="lg"
+                color="#909191"
+              />
+            </div>
+            <div>
+              <div>
+                <img
+                  src={constants.USER_UNDEFINE}
+                  alt="a"
+                  className="image-user"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
