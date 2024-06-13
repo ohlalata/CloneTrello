0ad1bdd2eff3c5359e696a09cc5a75e859062261
Toan Nguyen Bao <toan.nguyen@titancorp.vn>
2024-06-13 15:30:22 +0700

    confirm delete board

--- clone-trello/src/components/navBarBoardContent/index.js
+++ clone-trello/src/components/navBarBoardContent/index.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import boardService from "../../api/Services/board";
 
-const NavbarBoardContent = () => {
+const NavbarBoardContent = (boardID) => {
+  //console.log(boardID.boardID);
+  const [boardName, setBoardName] = useState("");
+
+  const [listBoardName, setListBoardName] = useState([]);
+
+  const handleGetAllBoard = async () => {
+    try {
+      const response = await boardService.getAllBoard();
+      if (response.data.code == 200) {
+        const result = response.data.data;
+        setBoardName(
+          result.filter((board) => board.id == boardID.boardID)[0].name
+        );
+        setListBoardName(result);
+        console.log(result);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetAllBoard();
+  }, []);
+
   return (
     <React.Fragment>
       <nav className="navbar navbar__board-content">
         <div className="container-fluid d-flex justify-content-between">
           <div className="d-flex gap-3">
-            <div>
-              <span>Board Name</span>
+            <div className="d-flex align-items-center">
+              <span className="fs-5 fw-bold" style={{ color: "#455570" }}>
+                {boardName}
+              </span>
             </div>
             <div className="d-flex gap-2 block__change-visibility p-1 align-items-center">
               <span>
