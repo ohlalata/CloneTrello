6f4b3015d92498d78a9903ad4adc682a3d5b032f
Phong Nguyen Thai <phong.nguyenth@titancorpvn.com>
2024-07-31 17:17:47 +0700

    Update animation

--- clone-trello/src/components/card/index.js
+++ clone-trello/src/components/card/index.js
@@ -1145,7 +1145,7 @@ const Card = (listIdProps, listBoardIdProps) => {
 
   useEffect(() => {
     handleGetListByFilter();
-  }, []);
+  }, [listIdProps,  listBoardIdProps]); // Add dependencies here
 
   const handleListChange = (e) => {
     setSelectedListId(e.target.value);
